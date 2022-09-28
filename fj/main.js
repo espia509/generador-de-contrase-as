12f9cbd4a253dc980b3843ea3b676830865c21b3
Daniel Gonzalez Escalante <48493268+espia509@users.noreply.github.com>
2022-09-28 17:55:07 -0500

    Add files via upload

--- fj/main.js
+++ fj/main.js
@@ -1,6 +1,10 @@
+const gop = document.getElementById("lot");
+const sho = document.getElementById("lock").vale;
 const pass_gen = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890#$%&/=?@";
 
-function passGeneret(length = 10 || sho){
+
+function passGeneret(length = sho){
+    
     let result = "";
     for (let i = 0; i <= length; i++) {
         result += pass_gen.charAt(Math.floor(Math.random() * pass_gen.length))
@@ -8,14 +12,8 @@ function passGeneret(length = 10 || sho){
     return result;
 }
 
-
-const sho = document.getElementById("lock");
-const mos = document.getElementsByTagName("tr");
-
-onclick = passGeneret();
-onshow = passGeneret();
-
-app.elements.namedItem('btn').addEventListener('click', passGeneret());
-
+document.getElementById('btn1').onclick = function(){
+    gop.innerHTML=passGeneret();
+}
 
 
